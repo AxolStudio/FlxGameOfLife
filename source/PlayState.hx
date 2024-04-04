42d0557c3f27eeb38b560f662b82ce4e6cf08ace
Tim I Hely <seifertim@gmail.com>
2024-04-04 14:52:20 -0500

    still working

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.math.FlxPoint;
 import flixel.text.FlxText;
 import flixel.tile.FlxBaseTilemap.FlxTilemapAutoTiling;
 import flixel.tile.FlxTilemap;
@@ -25,6 +26,15 @@ class PlayState extends FlxState
 
 	// our map of life
 	public var lifeMap:FlxTilemap;
+
+	// which mode the mouse is in
+	public var mouseMode:MouseMode = MouseMode.NONE;
+
+	// the speed of the simulation
+	public var speed:Int = 0;
+
+	// the different speeds we can use, in seconds
+	public static var SPEEDS:Array<Float> = [1, .75, .5, .25];
 
 	override public function create()
 	{
@@ -99,24 +109,32 @@ class PlayState extends FlxState
 		var midX:Int = Std.int((endX - startX) / 2);
 		var yPos:Float = 8;
 
-		// speed down button
-		var btn:FlxButton = new FlxButton(0, yPos, "-", speedDown);
-		btn.x = startX + midX - (btn.width / 2);
-		add(btn);
-
-		yPos += btn.height + 2;
-
-		// current speed
-		var txt:FlxText = new FlxText(startX, yPos, endX - startX, "1");
+		var txt:FlxText = new FlxText(startX, yPos, endX - startX, "SPEED");
 		txt.color = FlxColor.WHITE;
 		txt.alignment = FlxTextAlign.CENTER;
 		txt.x = startX + midX - (txt.width / 2);
 		add(txt);
 
-		yPos += txt.height + 2;
+		yPos += txt.height + 4;
+
+		// speed down button
+		var btn:FlxButton = new FlxButton(0, yPos, "<", speedDown);
+		btn.x = startX + midX - (btn.width / 2);
+		add(btn);
+
+		yPos += btn.height + 4;
+
+		// current speed
+		txt = new FlxText(startX, yPos, endX - startX, ">");
+		txt.color = FlxColor.WHITE;
+		txt.alignment = FlxTextAlign.CENTER;
+		txt.x = startX + midX - (txt.width / 2);
+		add(txt);
+
+		yPos += txt.height + 4;
 
 		// speed up button
-		btn = new FlxButton(startX, yPos, "+", speedUp);
+		btn = new FlxButton(startX, yPos, ">", speedUp);
 		btn.x = startX + midX - (btn.width / 2);
 		add(btn);
 
@@ -127,17 +145,41 @@ class PlayState extends FlxState
 		btn.x = startX + midX - (btn.width / 2);
 		add(btn);
 
+		yPos += btn.height + 16;
+
 		// preset 1 button
-		// preset 2 button
-		// preset 3 button
+		btn = new FlxButton(startX, yPos, "PRESET 1", selectPreset.bind(0));
+		btn.x = startX + midX - (btn.width / 2);
+		add(btn);
 
 		yPos += btn.height + 4;
 
+		// preset 2 button
+		btn = new FlxButton(startX, yPos, "PRESET 2", selectPreset.bind(1));
+		btn.x = startX + midX - (btn.width / 2);
+		add(btn);
+
+		yPos += btn.height + 4;
+
+		// preset 3 button
+
+		btn = new FlxButton(startX, yPos, "PRESET 3", selectPreset.bind(2));
+		btn.x = startX + midX - (btn.width / 2);
+		add(btn);
+
+		yPos += btn.height + 16;
+
+		// clear button
 		btn = new FlxButton(startX, yPos, "CLEAR", clearMap);
 		btn.x = startX + midX - (btn.width / 2);
 		add(btn);
 
+		yPos += btn.height + 16;
+
 		// show rules button
+		btn = new FlxButton(startX, yPos, "RULES", showRules);
+		btn.x = startX + midX - (btn.width / 2);
+		add(btn);
 	}
 
 	private function clearMap():Void {}
@@ -148,8 +190,47 @@ class PlayState extends FlxState
 
 	private function pauseGame():Void {}
 
+	private function selectPreset(preset:Int):Void {}
+
+	private function showRules():Void {}
+
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		updateMouse();
+
+		updateSimulation(elapsed);
 	}
+
+	private function updateSimulation(elapsed:Float):Void {}
+
+	private function updateMouse():Void
+	{
+		final mPos:FlxPoint = FlxG.mouse.getWorldPosition();
+		if (mPos.x >= lifeMap.x && mPos.y >= lifeMap.y && mPos.x < lifeMap.x + lifeMap.width && mPos.y < lifeMap.y + lifeMap.height)
+		{
+			var mPosOff:FlxPoint = new FlxPoint((mPos.x - lifeMap.x) / TILE_SIZE, (mPos.y - lifeMap.y) / TILE_SIZE);
+
+			if (FlxG.mouse.justPressed)
+			{
+				mouseMode = lifeMap.getTile(Std.int(mPosOff.x), Std.int(mPosOff.y)) == 0 ? PAINT : ERASE;
+			}
+			else if (FlxG.mouse.pressed)
+			{
+				lifeMap.setTile(Std.int(mPosOff.x), Std.int(mPosOff.y), mouseMode == PAINT ? 1 : 0);
+			}
+			else if (FlxG.mouse.released)
+				mouseMode = NONE;
+		}
+		else
+			mouseMode = NONE;
+	}
+}
+
+enum MouseMode
+{
+	NONE;
+	PAINT;
+	ERASE;
 }
